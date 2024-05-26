554887a15b403b3b4a0d3a261d02014622efbc4e
zishanAnsari <142493670+1337-pr05@users.noreply.github.com>
2024-05-26 23:40:25 +0530

    Add files via upload

--- front.js
+++ front.js
@@ -1,60 +1,62 @@
 var f;
-var x=1800;
-var y=425;
-var _x=1;//1800
+var x=1950;
+var y=625;
+var _x=1;
 var _y=0;
 var __x=1800;
 var __y=425;
+var cnv;
 function setup(){
-    createCanvas(windowWidth,windowHeight);
-    f=windowWidth;
+     cnv= createCanvas(windowWidth,windowHeight);
+     cnv.position(0,0);
+     cnv.style("z-index","-1");   
+    //  cnv.style("height","100%");
+    //  cnv.style("width","100%");
+     
+
 }
 function draw(){
    background(0);
    stroke(225);
-   strokeWeight(20);
-   if(x==1800 && y==425){
-    _x=0;
-    _y=-10;
+   strokeWeight(6);
+   if(x==1950 && y==625){
+    _x=-10;
+    _y=0;
   }
-  if(y==225 && x==1800){
+  if(y==625 && x==1750){
+    _y=-10;
+    _x=0;
+  }
+  if(x==1750  && y==275 )
+    {
+    _x=-10;
+    _y=0;
+    }
+  if(x==1000 && y==275){
+    _y=10;
+    _x=0
+  }
+  if(x==1000 && y==625){
     _y=0;
     _x=-10;
-  }
-  if(x==950 && y==225 )
-    {
-    _x=0;
-    _y=10;
-    }
-  if(x==950 && y==425){
-    _y=0;
-    _x=-10
-  }
-  if(x==780 && y==425){
-    _y=10;
-    _x=0;
 }
-    if(x==780 && y==675){
-        _x=10;
-        _y=0;
+    if(x==800 && y==625){
+        _x=0;
+        _y=10;
 
     }
-    if(x==1950 && y==675){
-      _y=-10;
-      _x=0;
-    }
-    if(x==1950 && y==425){
-      _x=-10;
+    if(x==800 && y==825){
       _y=0;
+      _x=10;
     }
-  //point(windowWidth-100,425);
+    if(x==1950 && y==825){
+      _x=0;
+      _y=-10;
+    }
   point(x,y)
-//  point(__x--,__y);
-  // point(x,y);
-//   point(_x,y);
   x+=_x;
   y+=_y;
-strokeWeight(10);
+strokeWeight(5);
   for (var i=0;i<15;i++){
     point(random(windowWidth),random(windowHeight));
   }
